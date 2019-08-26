d978a1ae64e36a18a3461801b012eaae431257e0
vlad.svichar@neudesic.com <vlad.svichar@neudesic.com>
2019-08-26 08:39:50 -0700

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Second middleware');
-    res.send('Hello from the Express!');
+app.use('/api/posts', (req, res, next) => {
+    //res.send('Hello from the Express!');
+    const posts = [
+        { 
+            id: 'asda123d', 
+            title:"First server-side post",
+            content: "This is coming from the server"
+        },
+        { 
+            id: 'asda123e', 
+            title:"Seond server-side post",
+            content: "This is coming from the server"
+        }
+    ]
+    res.status(200).json(
+        {
+            message: 'Posts fetched successfully!',
+            posts: posts
+        }
+    );
 });
 
 module.exports = app;
